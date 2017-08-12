4c8db79054c7bb4de5aa6a4da062f9a768358880
Ünsal Sercan Küliğ <sercankulig@gmail.com>
2017-08-12 23:18:39 +0300

    childroot works with click

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -9,6 +9,7 @@ import { Recipe } from '../../../models/recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
+  @Input() index: number;
 
   ngOnInit() {
   }
