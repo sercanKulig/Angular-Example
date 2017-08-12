3c5e31983e62d8c4c46b4a6e973a6ce81feb3b4d
Ünsal Sercan Küliğ <sercankulig@gmail.com>
2017-08-12 14:00:16 +0300

    Model and using it

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'Test' ,'http://images.media-allrecipes.com/userphotos/250x250/2160152.jpg'),
+    new Recipe('A Test Recipe', 'Test' ,'http://images.media-allrecipes.com/userphotos/250x250/2160152.jpg'),
+    new Recipe('A Test Recipe', 'Test' ,'http://images.media-allrecipes.com/userphotos/250x250/2160152.jpg')
+  ];
 
   constructor() { }
 
