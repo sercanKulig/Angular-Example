eef25ef1e65e9a8b9e073bf344c13970f3247a74
Ünsal Sercan Küliğ <sercankulig@gmail.com>
2017-08-12 20:56:18 +0300

    add model to another

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,12 +1,25 @@
 import {Recipe} from "../models/recipe.model";
 import {EventEmitter} from "@angular/core";
+import {Ingredient} from "app/models/ingredient.model";
 
 export class RecipeService {
-  recipeSelected  = new EventEmitter<Recipe>();
+  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe('A Test Recipe',
+      'This is simply a test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('Another Test Recipe',
+      'This is simply a test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
   ];
 
   getRecipes() {
