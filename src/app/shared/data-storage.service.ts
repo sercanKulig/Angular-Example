f79cbc32a410881970ed840aca5656d958f8f465
sercanKuliq <sercan.kulig@gmail.com>
2017-08-18 17:50:38 +0300

    set missing Indigriant

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,22 +2,36 @@ import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
 import {RecipeService} from '../services/recipe.service';
 import {Recipe} from '../models/recipe.model';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: Http, private recipeService: RecipeService) {}
+  constructor(private http: Http, private recipeService: RecipeService) {
+  }
 
   storeRecipes() {
     return this.http.put('https://angular-http-b118b.firebaseio.com/recipes.json', this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    this.http.get('https://angular-http-b118b.firebaseio.com/recipes.json').
-      subscribe(
-      (response: Response) => {
-        const recipes: Recipe[] = response.json();
-        this.recipeService.setRecipes(recipes);
-      }
-    );
+    this.http.get('https://angular-http-b118b.firebaseio.com/recipes.json')
+      .map(
+        (response: Response) => {
+          const recipes: Recipe[] = response.json();
+          for (let recipe of recipes) {
+            if (!recipe['ingredients']) {
+              recipe['ingredients'] = [];
+            }
+          }
+          return recipes;
+        }
+      )
+      .subscribe(
+        (recipes: Recipe[]) => {
+
+          this.recipeService.setRecipes(recipes);
+        }
+      );
   }
+
 }
